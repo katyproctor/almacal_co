39e21305804db1bf63b1e37d14b5c226aa3ba3cc
Katy Proctor <katyproctor@icloud.com>
2021-02-05 13:48:01 +0800

    Adding individual imaging quality check and combined image code

---
+++ quality_check.py
@@ -0,0 +1,111 @@
+### Check data quality of individual UIDs
+### Script subtracts continuum for relevant spw
+### images each individually
+### plots uv-coverage & outputs rms/beam properties
+
+import os
+import re
+import json
+import numpy as np
+import matplotlib.pyplot as plt 
+import analysisUtils as au
+import csv
+
+def read_json(file):
+    with open(file) as f:
+        data = json.load(f)
+    return data
+
+def image(data_path, uid, spw_list_str,imgname):
+    '''Clean the continuum subtracted vis for all uids'''
+    vis = data_path+uid 
+    
+    tclean(vis = vis,
+           spw = spw_list_str,
+           weighting = 'natural',
+       imagename = data_path+'katy_imaging/'+imgname,
+       specmode = 'cube')    
+    
+def plot_uv(data_path,uid, spw):
+    '''Plot uv-coverage for a given uid'''
+    plotms(vis = data_path+uid, spw = spw,
+           xaxis='u', yaxis='v',
+           averagedata=True, avgchannel='',
+          plotfile = '/home/kproctor/uv_plots/' + uid + spw + '.png', 
+            expformat = 'png', exprange = 'all', overwrite=True)
+
+def get_uv_stats(data_path, uid):
+
+    uvstats = visstat(data_path+uid, axis='uvrange', spw = '0') 
+    min_bl = uvstats['DATA_DESC_ID=0']['min']
+    max_bl = uvstats['DATA_DESC_ID=0']['max']
+    med_bl = uvstats['DATA_DESC_ID=0']['median']
+    mean_bl = uvstats['DATA_DESC_ID=0']['mean']
+    stddev_bl = uvstats['DATA_DESC_ID=0']['stddev']
+
+    return [min_bl, max_bl, med_bl, mean_bl, stddev_bl]
+   
+def get_img_stats(data_path,imgname):
+    '''Get rms, major and minor beam axis of clean image'''
+    imgfilename = data_path+'katy_imaging/'+imgname+'.image'
+    rms = imstat(imgfilename)['rms'][0]      
+    # resolution info
+    maj_beam_val = imhead(imgfilename, mode='list')['perplanebeams']['median area beam']['major']['value']
+    maj_beam_unit = imhead(imgfilename, mode='list')['perplanebeams']['median area beam']['major']['unit']
+    
+    min_beam_val = imhead(imgfilename, mode='list')['perplanebeams']['median area beam']['minor']['value']
+    min_beam_unit = imhead(imgfilename, mode='list')['perplanebeams']['median area beam']['minor']['unit']
+    
+    return [imgname, rms, maj_beam_val, maj_beam_unit,
+            min_beam_val, min_beam_unit]
+
+def output_to_csv(stats):
+    with open("/home/kproctor/summary_stats_all.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["image", "rms", "maj_beam", "units",
+                    "min_beam", "units"])
+        writer.writerows(stats)
+
+def spw_list_to_str(spw_list):
+    '''Convert spw/list of spws to strings for tclean input'''
+    
+    spw_str = [',']*len(spw_list)
+
+    for i in range(len(spw_list)):
+        if isinstance(spw_list[i], list):
+            spw_str[i] = str(','.join([str(elem) for elem in spw_list[i]]))
+        else:
+            spw_str[i] = str(spw_list[i])
+    return spw_str
+
+def main():
+    # read spw list
+    spw_list_file = 'J1229+0203_all.txt'
+    spw_data = read_json(spw_list_file)
+    
+    uid_list = spw_data['uid']
+    spw_list = spw_data['spwid']
+    spw_str = spw_list_to_str(spw_list)
+     
+    # where the visibility data is located 
+    data_path = '/scratch-sata/gcalistr/ALMACAL/J1229+0203/'
+    summary_stats = []
+    
+    # for every uid, image separately and get stats
+    for i, uid in enumerate(uid_list):
+        print('Imaging ', i,uid)
+        uid_str = str(uid)
+        imgname = uid_str + '_co'
+        
+        # image each uid individually
+        image(data_path, uid_str, spw_str[i], imgname)
+    
+        # gets stats and uv-coverage
+        uv_stats = get_uv_stats(data_path, uid_str)
+        summary_stats.append(get_img_stats(data_path,imgname)+uv_stats)
+        #plot_uv(data_path,uid_str, spw_str[i])
+        
+    output_to_csv(summary_stats)    
+    
+if __name__ == "__main__":
+    main()    
---
+++ spw_list.py
@@ -0,0 +1,181 @@
+# Modified version of code written by Roland
+# Finds relevant spws, given uid list and line emission frequency
+
+import os
+import re
+import json
+import numpy as np
+import matplotlib.pyplot as plt 
+import analysisUtils as au
+
+def read_spw(vis):
+    """read the spectral windows
+    """
+    if isinstance(vis, str):
+        vis = [vis, ]
+    
+    if not isinstance(vis, list):
+        raise ValueError("read_spw: Unsupported measurements files!")
+    
+    spw_specrange = {}
+
+    # Read the spectral window frequency ranges
+    for v in vis:
+        tb.open(v + '/SPECTRAL_WINDOW')
+        col_names = tb.getvarcol('NAME')
+        col_freq = tb.getvarcol('CHAN_FREQ')
+        tb.close()
+
+        for key in col_names.keys():
+            freq_max = np.max(col_freq[key]) / 1e9
+            freq_min = np.min(col_freq[key]) / 1e9
+            spw_specrange[key] = [freq_min, freq_max]
+
+    return spw_specrange
+
+
+def spw_stat(objfolder, vis=None, savedata=False, spw_list_path=None, z=0, lines=None, lines_names=None, debug=False):
+
+    spw_list = {'B3':{'name':[], 'time':[], 'freq':[], 'spwid':[]},
+                'B4':{'name':[], 'time':[], 'freq':[], 'spwid':[]},
+                'B5':{'name':[], 'time':[], 'freq':[], 'spwid':[]},
+                'B6':{'name':[], 'time':[], 'freq':[], 'spwid':[]},
+                'B7':{'name':[], 'time':[], 'freq':[], 'spwid':[]},
+                'B8':{'name':[], 'time':[], 'freq':[], 'spwid':[]},
+                'B9':{'name':[], 'time':[], 'freq':[], 'spwid':[]},
+                'B10':{'name':[], 'time':[], 'freq':[], 'spwid':[]},}
+    filelist = []
+
+    base_dir = objfolder
+    obj = os.path.basename(objfolder)
+
+    p_obs = re.compile('^uid___.*\d$')
+
+    for obs in os.listdir(base_dir):
+        if p_obs.match(obs):
+            filelist.append(os.path.join(base_dir, obs))
+
+    band_match = re.compile('_(?P<band>B\d{1,2})')
+
+    for obs in filelist:
+        try:
+            if band_match.search(obs):
+                band = band_match.search(obs).groupdict()['band']
+                if debug:
+                    print("Band: ", band)
+            else:
+                print("Error in band match.")
+
+            spw_list[band]['name'].append(os.path.basename(obs))
+            spw_specrange_buff = read_spw(obs)
+            spw_specrange = spw_specrange_buff.values()
+            spw_ids = spw_specrange_buff.keys()
+
+            # Fix SPW ids
+            for l in range(0, len(spw_ids)):
+                if(spw_ids[l] == 'r1'):
+                    spw_ids[l] = 0
+                elif(spw_ids[l] == 'r2'):
+                    spw_ids[l] = 1
+                elif(spw_ids[l] == 'r3'):
+                    spw_ids[l] = 2
+                elif(spw_ids[l] == 'r4'):
+                    spw_ids[l] = 3
+                elif(spw_ids[l] == 'r5'):
+                    spw_ids[l] = 4
+                elif(spw_ids[l] == 'r6'):
+                    spw_ids[l] = 5
+                elif(spw_ids[l] == 'r7'):
+                    spw_ids[l] = 6
+ 
+            spw_list[band]['freq'].append(list(spw_specrange))
+            spw_list[band]['spwid'].append(list(spw_ids))
+        except:
+            print("Error: in", obs)
+
+    if savedata:
+        with open(spw_list_path, 'w') as fp:
+            json.dump(spw_list, fp)
+    return spw_list
+
+
+#function to find overlap in SPW freq and line freq ranges
+def findOverlap(a, b, rtol = 1e-03, atol = 1e-03, equal_nan = False):
+    overlap_indexes = []
+    for i, item_a in enumerate(a):
+        for item_b in b:
+            if np.isclose(item_a, item_b, rtol = rtol, atol = atol, equal_nan = equal_nan):
+                overlap_indexes.append(i)
+    return overlap_indexes
+
+
+def create_spw_list(ms_folder, linerange, savedata=False, spw_list_path = None):
+
+	spw_stat_list = spw_stat(objfolder=ms_folder)
+	spw_list = {'uid':[], 'spwid':[]}
+
+	print('Read in SPW frequency ranges...')
+	for i in range(0, len(spw_stat_list['B3']['name'])):
+	    name = spw_stat_list['B3']['name'][i]
+	    freqs = spw_stat_list['B3']['freq'][i]
+	    spwid = spw_stat_list['B3']['spwid'][i]
+	    avail_spws = []
+	    spw_avail = False
+
+	    print('Checking frequency overlap for ' + str(name) + '...')
+	    for k in range(0, len(freqs)):
+	    	lin_arr = np.arange(linerange[0], linerange[1], step=0.1)
+	    	freq_arr = np.arange(np.round(min(freqs[k]),decimals=2), np.round(max(freqs[k]), decimals=2), step=0.1)
+	    	overlap = findOverlap(lin_arr, freq_arr)
+
+	        if len(overlap) > 0:
+	            avail_spws.append(spwid[k])
+	            spw_avail = True
+
+	    if spw_avail == True:
+	    	print('Frequency overlap for ' + str(name) + ' SPWs: ' + str(avail_spws))
+	    	print('')
+	        spw_list['uid'].append(name)
+	        spw_list['spwid'].append(avail_spws)
+	    else:
+	    	print('No frequency overlap for ' + str(name))
+	    	print('')
+	
+	print('SPW List:')
+	print(spw_list)
+
+	if savedata:
+		print('Saving SPW list in ' + str(spw_list_path))
+		with open(spw_list_path, 'w') as fp:
+			json.dump(spw_list, fp)
+
+	print('Creating spw_list is done! \n \n')
+	return spw_list
+
+def read_json(file):
+	with open(file) as f:
+		data = json.load(f)
+	return data
+
+
+def main():
+
+	# Set these two variables to the directory of the ms files
+	ms_file_path = '/scratch-sata/gcalistr/ALMACAL/J1229+0203/katy_test'
+	# Specify the range of the CO line [lower limit, upper limit] in GHz
+	linerange=[99.,100.]
+
+	# Use this part instead if you want the SPW list saved, will read the SPW list from that file too afterwards
+	spw_list_path = 'husemann_extra.txt'
+	create_spw_list(ms_folder=ms_file_path, linerange=linerange, savedata=True, spw_list_path=spw_list_path)
+	spw_list_file = 'husemann_extra.txt'
+	spw_data = read_json(spw_list_file)
+
+	#spw_data = create_spw_list(ms_folder=ms_file_path, linerange=linerange)
+
+	uid_list = spw_data['uid']
+	spw_list = spw_data['spwid']
+        
+        
+if __name__ == "__main__":
+    main()
