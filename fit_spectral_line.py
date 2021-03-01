a55383586a72677db12e63778963d712c789c1c2
Katy Proctor <katyproctor@icloud.com>
2021-03-01 14:20:46 +0800

    Add spectral line fitting code

--- fit_spectral_line.py
+++ fit_spectral_line.py
@@ -4,19 +4,19 @@ from astropy.io import fits
 from astropy.modeling import models, fitting
 from astropy.modeling.models import custom_model
 from astropy.modeling import Fittable1DModel, Parameter
+from scipy import integrate
 
-def limit_data(dat, low, upp):
+def limit_data(dat, low, upp, f0):
     # freq in GHz
     freq = dat[0]/1e9
     flux = dat[1]
     
     # use velocity rather than freq
-    f0 = 99.5
     c = 3e8
     # velocity in km/s
     v = ((f0 - freq)/f0*c)/1e3
     
-    # fit to frequency range between lower and upper limit
+    # fit to velocity range between lower and upper limit
     ind = np.where((v > low) & (v < upp))
 
     v = v[ind]
@@ -50,7 +50,7 @@ def fit_three_component(v, flux, a1, a2, v1, v2, s1, s2):
     triple_model = g1 + g2 + offset
 
     # set limits on location of peak
-    delta = 10 # in km/s
+    delta = 20 # in km/s
     triple_model.mean_0.max = v1 + delta
     triple_model.mean_0.min = v1 - delta
     triple_model.mean_1.max = v2 + delta
@@ -59,6 +59,25 @@ def fit_three_component(v, flux, a1, a2, v1, v2, s1, s2):
     # perform fit
     fitter = fitting.LevMarLSQFitter()
     gaussian_fit = fitter(triple_model, v, flux)
+    
+    return gaussian_fit
+
+def fit_gaussian(v, flux, a1, v1, s1):   
+    
+    # 2 gaussian components + vertical offset to account for issues in cont sub
+    g1 = models.Gaussian1D(a1, v1, s1) # amplitude, vel, standard dev.
+    offset = models.Polynomial1D(degree=0)
+    
+    double_model = g1 + offset
+
+    # set limits on location of peak
+    delta = 20 # in km/s
+    double_model.mean_0.max = v1 + delta
+    double_model.mean_0.min = v1 - delta
+    
+    # perform fit
+    fitter = fitting.LevMarLSQFitter()
+    gaussian_fit = fitter(double_model, v, flux)
     
     return gaussian_fit
 
@@ -76,32 +95,62 @@ def plot_result(low, upp, vel, flux, gaussian_fit, outname):
     
     plt.savefig('plots/'+ str(outname) + '_fit_result.png', dpi = 300)
     
+def calc_flux(model, lowlim, uplim):
+    '''return flux value in Jy.km/s'''
+    # account for vertical offset
+    offset = model[-1](lowlim)
+    
+    def offset_model(x):
+        return model(x) - offset
+    
+    return integrate.quad(offset_model, lowlim, uplim)
+    
     
 def main():
     # load data and specify plot name
-    dat = np.loadtxt('../data/J1229+0203/co1_0_husemann_improved_highres.image_spectra_mask.txt').T
-    outname = 'husemann_improved_highres'
+    dat = np.loadtxt('../data/J1229+0203/co2_1_omission.image_spectra_mask.txt').T
+    outname = 'co2_1_omission'
     
     # range for fitting in km/s
-    upp = 1400
-    low = -700
+    upp = 400
+    low = -1000
+    
+    # observed frequency of line transition
+    f0 = 199.0846
     
     # limit and plot data
-    vel, flux = limit_data(dat, low, upp)
+    vel, flux = limit_data(dat, low, upp, f0)
     
     # provide initial guesses for fit values
-    a1 = 0.004
-    a2 = 0.0025
-    v1 = -30
-    v2 = 240
-    s1 = 70
-    s2 = 70
+    a1 = 0.01
+    a2 = 0.01
+    v1 = 125
+    v2 = 350
+    s1 = 130
+    s2 = 50
+    
+    a1 = 0.008
+    s1 = 210
+    v1 = 100
     
     plot_spectra(v1, v2, vel, flux, outname)
     
     # fit spectral line and plot
-    model = fit_three_component(vel, flux, a1, a2, v1, v2, s1, s2)
+    # use this for double gaussian
+    #model = fit_three_component(vel, flux, a1, a2, v1, v2, s1, s2)
+    
+    
+    # use this for single gaussian (co(2-1))
+    model = fit_gaussian(vel, flux, a1, v1, s1)
+    print(model[0], model[1])
+    
     plot_result(low, upp, vel, flux, model, outname)
+    
+    # get flux between integral limits
+    flux_up = 400
+    flux_low = -100
+    flux = calc_flux(model, flux_low, flux_up)
+    print('Total flux = ', flux[0], ' Jy.km/s')
     
 if __name__ == "__main__":
     main()    
